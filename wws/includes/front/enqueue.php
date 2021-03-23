f19c28c79275755902b8c0d5cfa30fbc0263b8e0
G3N <yap@corekara.co.jp>
2021-03-23 09:32:38 +0900

    save file sth wrong with the sass

--- wws/includes/front/enqueue.php
+++ wws/includes/front/enqueue.php
@@ -8,4 +8,20 @@ function wws_enqueue() {
   wp_register_style('amaranth_google_font', 'https://fonts.googleapis.com/css2?family=Amaranth:ital,wght@0,400;0,700;1,400;1,700&display=swap');
   wp_register_style('abril_google_font', 'https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap');
   wp_register_style('fraunces_google_font', 'https://fonts.googleapis.com/css2?family=Fraunces:wght@500&display=swap');
+  wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css');
+  wp_register_style('bootstrap_icon', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css');
+  wp_register_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css');
+  wp_register_style('aos_style', 'https://unpkg.com/aos@2.3.1/dist/aos.css');
+  wp_register_style('styles', $uri . '/assets/css/styles.css');
+
+  wp_enqueue_style('heebo_google_font');
+  wp_enqueue_style('alegreya_google_font');
+  wp_enqueue_style('amaranth_google_font');
+  wp_enqueue_style('abril_google_font');
+  wp_enqueue_style('fraunces_google_font');
+  wp_enqueue_style('bootstrap');
+  wp_enqueue_style('bootstrap_icon');
+  wp_enqueue_style('fontawesome');
+  wp_enqueue_style('aos_style');
+  wp_enqueue_style('styles');
 }
--- wws/index.php
+++ wws/index.php
@@ -3,13 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="./main.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
-    crossorigin="anonymous" />
-  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <?php wp_head(); ?>
   <title>WWS</title>
 </head>
 <body>
